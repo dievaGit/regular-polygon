9c714d544b2037d514300d91a232ce8ef9fb275b
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 21:40:37 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaPerimetroPoligonoRegular
+{
+    class Program
+    {
+        // Funcion que calcula perimetro de poligono regular
+        static float PerimetroPoligono(float lad, float cant)
+        {
+            float per = 0.0f;
+
+            per = lad * cant;
+
+            return per;
+        }
+
+        // Funcion principal
+        static void Main(string[] args)
+        {
+            float lado = 0.0f, cantidad = 0.0f, perimetro = 0.0f;
+
+            Console.Write("Escriba el tamaño del lado: ");
+            lado = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Escriba la cantidad de lados:");
+            cantidad = Convert.ToSingle(Console.ReadLine());
+
+            perimetro = PerimetroPoligono(lado, cantidad);
+
+            Console.WriteLine("El perimetro del poligono es: {0}", perimetro);
+
+            Console.ReadKey();
+
+        }
+    }
+}
